f797b51a97e3571dcbbe226a1c3956d5b3236328
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-11-23 13:31:29 -0500

    update port

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running on port 5000');
+    console.log('Server is running on port '+ PORT);
 });
 
 // STuF4EFC5rUJkIsp 
