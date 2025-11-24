a3dd8834922822ced621c3b657e411f75f11c4d4
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-11-24 02:00:53 -0500

    fix: deployment

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,14 @@ const __dirname = path.resolve();
 
 app.use(express.json()); // middleware to parse JSON request bodies, allows us to accept JSON data
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    })
+    });
 }
+
 
 // use the product routes for any requests to /api/products
 app.use("/api/products", productRoutes);
@@ -31,6 +32,3 @@ app.listen(PORT, () => {
     connectDB();
     console.log('Server is running on port '+ PORT);
 });
-
-// STuF4EFC5rUJkIsp 
-// vrindajoshi30_db_user
